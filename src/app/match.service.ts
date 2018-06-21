66ce41eaeeba8e67b8e6568d0245115be651a332
Andor233 <andor.liu@gmail.com>
2018-06-21 16:00:21 +0800

    fix bugs

--- src/app/match.service.ts
+++ src/app/match.service.ts
@@ -12,6 +12,7 @@ export class MatchService {
   private url = 'https://worldcup.sfg.io/matches/';
 
   constructor(private http: HttpClient) {
+    this.allMatches = this.getAllMatches().pipe(shareReplay());
   }
 
   getAllMatchesWithDetails(): Observable<Match[]> {
