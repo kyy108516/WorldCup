d91477cd9120f673cdd1c53905a3c4e830d69922
Andor233 <andor.liu@gmail.com>
2018-06-27 13:21:10 +0800

    fix bugs

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -53,8 +53,8 @@ export class TeamComponent implements OnInit {
       this.teamService.groupResult
         .subscribe(teams => {
           this.flag = true;
-          this.teams = teams[id].group.teams.map(it => it.team);
-          this.tempTeams = this.teams.slice(0, this.cnt);
+          this.tempTeams = teams[id].ordered_teams;
+          this.teams = this.tempTeams;
         });
     }
   }
